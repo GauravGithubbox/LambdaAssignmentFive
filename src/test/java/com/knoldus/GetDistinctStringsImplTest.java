9cc4a434c734711736b67a4b367a34196696facf
GauravGithubbox <76477015+GauravGithubbox@users.noreply.github.com>
2021-03-11 21:23:17 +0530

    Create GetDistinctStringsImplTest.java

--- src/test/java/com/knoldus/GetDistinctStringsImplTest.java
+++ src/test/java/com/knoldus/GetDistinctStringsImplTest.java
@@ -18,7 +18,31 @@ class GetDistinctStringsImplTest {
 
         Assertions.assertEquals(strList,Uniqueset);
     }
+     @Test
+    public void similarStringTest() {
+        GetDistinctStringsImpl obj1 = new GetDistinctStringsImpl();
+        List<String> Uniqueset = obj1.get.distinctStrings(new ArrayList<>(Arrays.asList("java", "java", "c")));
+        List<String> strList = new ArrayList<>();
+        strList.add("c");
+        strList.add("c");
+        Assertions.assertNotEquals(strList, Uniqueset);
+
+    }
 
 
+
+
+    @Test
+    public void checkForNull() {
+        GetDistinctStringsImpl obj1 = new GetDistinctStringsImpl();
+        Assertions.assertThrows(NullPointerException.class, () -> {
+            obj1.get.distinctStrings(new ArrayList<>(Arrays.asList(null)));
+        });
+
+
+
+    }
 
 }
+
+
